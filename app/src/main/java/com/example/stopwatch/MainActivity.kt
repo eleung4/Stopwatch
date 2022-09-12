0a127b66c1b927a938cd5e1bd770c69d6d81c07f
ellen <ellenleung4@gmail.com>
2022-09-12 10:00:20 -0700

    wire widgets

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.stopwatch
 
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +14,25 @@ class MainActivity : AppCompatActivity() {
         //all your static constants go here
         val TAG = ""
     }
+
+    lateinit var start : Button
+    lateinit var reset : Button
+    lateinit var chro : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        wirewidgets()
+
+
+
+    }
+
+    private fun wirewidgets() {
+        start = findViewById(R.id.button_main_start)
+        reset = findViewById(R.id.button_main_reset)
+        chro = findViewById(R.id.chronometer_main_stopwatch)
     }
 
 
@@ -47,4 +66,6 @@ class MainActivity : AppCompatActivity() {
         super.onRestart()
         Log.d(TAG, "onRestart")
     }
+
+
 }
